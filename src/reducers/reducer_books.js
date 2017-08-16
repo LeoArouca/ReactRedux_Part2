8122935f7711ece9997bafc6d1ec0002925d9e47
LeoArouca <leo.arouca@purecontrolpilates.com>
2017-08-16 22:10:05 +0800

    Makes it work but checking null

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -2,10 +2,10 @@
 export default function(){
   return(
     [
-      {title: 'Javascript'},
-      {title: 'HP'},
-      {title: 'Dark'},
-      {title: 'Nikko'}
+      {title: 'Javascript', pages: 101},
+      {title: 'HP', pages: 29},
+      {title: 'Dark', pages: 85},
+      {title: 'Nikko', pages: 1}
     ]
   );
 }
